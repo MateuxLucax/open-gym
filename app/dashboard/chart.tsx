66322f6dcafdb1d448b77e64cdf8e857c33b024f
Mateus Lucas <mateuxlucax@gmail.com>
2023-11-30 18:36:17 -0300

    feat: add home screen

--- app/dashboard/chart.tsx
+++ app/dashboard/chart.tsx
@@ -1,55 +1,79 @@
 'use client';
 
 import { Card, AreaChart, Title, Text } from '@tremor/react';
+import { formatMoney } from '../utils';
 
-const data = [
+export const data = [
   {
-    Month: 'Jan 21',
-    Sales: 2890,
-    Profit: 2400
+    Mês: 'Jan 23',
+    Entrada: 16584.04,
+    Saída: 5349.12
   },
   {
-    Month: 'Feb 21',
-    Sales: 1890,
-    Profit: 1390
+    Mês: 'Fev 23',
+    Entrada: 19382.54,
+    Saída: 6238.32
   },
   {
-    Month: 'Mar 21',
-    Sales: 4230,
-    Profit: 3000
+    Mês: 'Marc 23',
+    Entrada: 12230.98,
+    Saída: 5023.47
   },
   {
-    Month: 'Apr 21',
-    Sales: 3800,
-    Profit: 2400
+    Mês: 'Abr 23',
+    Entrada: 14230.21,
+    Saída: 6980.35
   },
   {
-    Month: 'May 21',
-    Sales: 1290,
-    Profit: 760
+    Mês: 'Mai 23',
+    Entrada: 13230.54,
+    Saída: 6980.81
   },
   {
-    Month: 'Jun 21',
-    Sales: 1080,
-    Profit: 230
+    Mês: 'Jun 23',
+    Entrada: 13890.17,
+    Saída: 3400.63
+  },
+  {
+    Mês: 'Jul 23',
+    Entrada: 14890.91,
+    Saída: 7390.25
+  },
+  {
+    Mês: 'Ago 23',
+    Entrada: 18230.25,
+    Saída: 3493.64
+  },
+  {
+    Mês: 'Set 23',
+    Entrada: 17800.51,
+    Saída: 7800.53
+  },
+  {
+    Mês: 'Out 23',
+    Entrada: 14290.19,
+    Saída: 4360.95
+  },
+  {
+    Mês: 'Nov 23',
+    Entrada: 13080.61,
+    Saída: 4300.53
   }
 ];
 
 export default function Example() {
   return (
     <Card className="mt-8">
-      <Title>Performance</Title>
-      <Text>Comparison between Sales and Profit</Text>
+      <Title>Fluxo de Caixa</Title>
+      <Text>Comparção entre Entradas e Entradas</Text>
       <AreaChart
         className="mt-4 h-80"
         data={data}
-        categories={['Sales', 'Profit']}
-        index="Month"
-        colors={['indigo', 'fuchsia']}
-        valueFormatter={(number: number) =>
-          `$ ${Intl.NumberFormat('us').format(number).toString()}`
-        }
-        yAxisWidth={60}
+        categories={['Entrada', 'Saída']}
+        index="Mês"
+        colors={['red', 'blue']}
+        valueFormatter={formatMoney}
+        yAxisWidth={96}
       />
     </Card>
   );
